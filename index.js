7e3e40a51d64d0227b3f3b927c2805c91e067c0d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-01-15 20:56:27 +0100

    Write output to a file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var path = require('path');
+var markdown = require('markdown').markdown;
 
 var config = require('./config.json');
 
@@ -12,7 +13,17 @@ fs.readdir(config.POSTS_DIRECTORY, function(error, data){
         console.log(fileName);
         fs.readFile(fileName, 'utf8', function(error, data){
             if (error) throw error;
+
+            var output = markdown.toHTML(data);
             console.log(data);
+            console.log(output);
+
+            var outputName = path.join(config.BLOG_DIRECTORY, path.basename(post, '.md') + '.html');
+            fs.writeFile(outputName, output, 'utf8', function(error){
+                if (error) throw error;
+
+                console.log('finished writing');
+            });
         });
     });
 });
