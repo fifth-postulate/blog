6269f598cb4d5518063076c66c694fda12408e9d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-01-15 20:21:19 +0100

    List full path of post

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var fs = require('fs');
+var path = require('path');
 
 var config = require('./config.json');
 
 fs.readdir(config.POSTS_DIRECTORY, function(error, data){
     if (error) throw error;
 
-    console.log(data);
+    data.forEach(function(post){
+        var fileName = path.join(config.POSTS_DIRECTORY, post);
+
+        console.log(fileName);
+    });
 });
