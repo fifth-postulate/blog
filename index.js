2a8d28454f49e0611266911b1e0ee28d91a8dca8
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-01-15 21:02:28 +0100

    Remove console.logs

--- index.js
+++ index.js
@@ -10,13 +10,10 @@ fs.readdir(config.POSTS_DIRECTORY, function(error, data){
     data.forEach(function(post){
         var fileName = path.join(config.POSTS_DIRECTORY, post);
 
-        console.log(fileName);
         fs.readFile(fileName, 'utf8', function(error, data){
             if (error) throw error;
 
             var output = markdown.toHTML(data);
-            console.log(data);
-            console.log(output);
 
             var outputName = path.join(config.BLOG_DIRECTORY, path.basename(post, '.md') + '.html');
             fs.writeFile(outputName, output, 'utf8', function(error){
