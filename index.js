1dbe1a3ff0becddd0b728c0bc5fde54540fb0701
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-01-17 17:15:24 +0100

    Port index.js to promised-io/fs

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-var fs = require('fs');
 var path = require('path');
 var markdown = require('markdown').markdown;
+var qfs = require('promised-io/fs');
 
 var config = require('./config.json');
 
-fs.readdir(config.POSTS_DIRECTORY, function(error, data){
-    if (error) throw error;
+qfs.readdir(config.POSTS_DIRECTORY)
+    .then(function(posts){
+        var input = '';
+        posts.forEach(function(post){
+            var link = path.basename(post, '.md') + '.html';
+            input += '* ';
+            input += '[' + post + '](' + link + ')\n';
+        });
+        var output = markdown.toHTML(input);
 
-    var input = '';
-    data.forEach(function(post){
-        var link = path.basename(post, '.md') + '.html';
-        input += '* ';
-        input += '[' + post + '](' + link + ')\n';
+        var outputName = path.join(config.BLOG_DIRECTORY, 'index.html');
+        return qfs.writeFile(outputName, output, 'utf8');
+    })
+    .then(function(){
+        console.log('finished writing index.html');
     });
-    var output = markdown.toHTML(input);
-
-    var outputName = path.join(config.BLOG_DIRECTORY, 'index.html');
-    fs.writeFile(outputName, output, 'utf8', function(error){
-        if (error) throw error;
-
-        console.log('finished writing ' + outputName);
-    });
-});
