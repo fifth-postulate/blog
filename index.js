d61bf9896efe06e9e0b7ca92ceede9c5c986e541
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-01-18 09:39:05 +0100

    Use handlebars to transform index

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 var path = require('path');
 var markdown = require('markdown').markdown;
 var qfs = require('promised-io/fs');
+var handlebars = require('handlebars');
 
 var config = require('./config.json');
 
-qfs.readdir(config.POSTS_DIRECTORY)
-    .then(function(posts){
-        var input = '';
-        posts.forEach(function(post){
-            var link = path.basename(post, '.md') + '.html';
-            input += '* ';
-            input += '[' + post + '](' + link + ')\n';
-        });
-        var output = markdown.toHTML(input);
-
-        var outputName = path.join(config.BLOG_DIRECTORY, 'index.html');
-        return qfs.writeFile(outputName, output, 'utf8');
+qfs.readFile(config.TEMPLATES.index, 'utf8')
+    .then(function(templateSource){
+        return handlebars.compile(templateSource);
     })
-    .then(function(){
-        console.log('finished writing index.html');
-    });
+    .then(function(template){
+        qfs.readdir(config.POSTS_DIRECTORY)
+            .then(function(posts){
+                var input = '';
+                posts.forEach(function(post){
+                    var link = path.basename(post, '.md') + '.html';
+                    input += '* ';
+                    input += '[' + post + '](' + link + ')\n';
+                });
+                var content = markdown.toHTML(input);
+                var output = template({ 'content': content });
+                var outputName = path.join(config.BLOG_DIRECTORY, 'index.html');
+                return qfs.writeFile(outputName, output, 'utf8');
+            })
+            .then(function(){
+                console.log('finished writing index.html');
+            });
+   });
